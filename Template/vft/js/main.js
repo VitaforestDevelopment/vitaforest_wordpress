496a5c4baaa53dd9ba3c4ce2ee244ce77ccad421
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-01-24 00:26:58 +0300

    Revert "Тестирование lazy load изображений"
    This reverts commit 6ccd647e245f3e05eccf45bece6c2d461a1c003c.

--- Template/vft/js/main.js
+++ Template/vft/js/main.js
@@ -618,38 +618,6 @@ if (categoryDescriptionContainer != null) {
 }
 
 
-// Image lazy load
-
-document.addEventListener("DOMContentLoaded", function () {
-  var lazyloadImages = document.querySelectorAll("img.attachment-woocommerce_thumbnail");
-  var lazyloadThrottleTimeout;
-
-  function lazyload() {
-    if (lazyloadThrottleTimeout) {
-      clearTimeout(lazyloadThrottleTimeout);
-    }
-
-    lazyloadThrottleTimeout = setTimeout(function () {
-      var scrollTop = window.pageYOffset;
-      lazyloadImages.forEach(function (img) {
-        if (img.offsetTop < (window.innerHeight + scrollTop)) {
-          img.src = img.dataset.src;
-          img.classList.remove('lazy');
-        }
-      });
-      if (lazyloadImages.length == 0) {
-        document.removeEventListener("scroll", lazyload);
-        window.removeEventListener("resize", lazyload);
-        window.removeEventListener("orientationChange", lazyload);
-      }
-    }, 20);
-  }
-
-  document.addEventListener("scroll", lazyload);
-  window.addEventListener("resize", lazyload);
-  window.addEventListener("orientationChange", lazyload);
-});
-
 
 
 
