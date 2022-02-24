9c02a838a3ef70e58865c8ca9a56b547f1123f2e
hateahate <stepanpavlenko@icloud.com>
2022-02-24 16:17:18 +0300

    Исправление вывода цены товара в инвойсе

--- Template/vft/woocommerce/pdf/vft-invoice/invoice.php
+++ Template/vft/woocommerce/pdf/vft-invoice/invoice.php
@@ -9,6 +9,46 @@
           <picture><source srcset="<?php echo get_bloginfo( 'template_directory' ); ?>/woocommerce/pdf/vft-invoice/src/img/logo.webp"type="image/webp"><img src="<?php echo get_bloginfo( 'template_directory' ); ?>/woocommerce/pdf/vft-invoice/src/img/logo.png" alt="" srcset="" /></picture>
         </div>
       </div>
+      <?
+      $order = wc_get_order( $order_id );
+
+      // Get the Customer ID (User ID)
+      $customer_id = $order->get_customer_id(); // Or $order->get_user_id();
+      
+      // Get the WP_User Object instance
+      $user = $order->get_user();
+      
+      // Get the WP_User roles and capabilities
+      $user_roles = $user->roles;
+      
+      // Get the Customer billing email
+      $billing_email  = $order->get_billing_email();
+      
+      // Get the Customer billing phone
+      $billing_phone  = $order->get_billing_phone();
+      
+      // Customer billing information details
+      $billing_first_name = $order->get_billing_first_name();
+      $billing_last_name  = $order->get_billing_last_name();
+      $billing_company    = $order->get_billing_company();
+      $billing_address_1  = $order->get_billing_address_1();
+      $billing_address_2  = $order->get_billing_address_2();
+      $billing_city       = $order->get_billing_city();
+      $billing_state      = $order->get_billing_state();
+      $billing_postcode   = $order->get_billing_postcode();
+      $billing_country    = $order->get_billing_country();
+      
+      // Customer shipping information details
+      $shipping_first_name = $order->get_shipping_first_name();
+      $shipping_last_name  = $order->get_shipping_last_name();
+      $shipping_company    = $order->get_shipping_company();
+      $shipping_address_1  = $order->get_shipping_address_1();
+      $shipping_address_2  = $order->get_shipping_address_2();
+      $shipping_city       = $order->get_shipping_city();
+      $shipping_state      = $order->get_shipping_state();
+      $shipping_postcode   = $order->get_shipping_postcode();
+      $shipping_country    = $order->get_shipping_country();
+      ?>
       <div class="body">
         <h1 class="title">PROFORMA INVOICE</h1>
         <table class="table billing-table">
@@ -60,7 +100,12 @@
           <?php foreach ( $this->get_order_items() as $item_id => $item ) : ?>
             <tr>
               <td><?php echo $item['name']; ?></td>
-              <td><?php echo $item['order_price']; ?></td>
+              <td><?php 
+              $baseVal = $item['order_price'];
+              $baseVal = preg_replace('/[^0-9]/', '', $baseVal);
+              $resultVal = $baseVal/$item['quantity'];
+              echo '€'.$resultVal;
+              ?></td>
               <td><?php echo $item['quantity']; ?></td>
               <td><?php echo $item['order_price']; ?></td>
             </tr>
