b00b1cee26e6be987f776f8c27daba6534b84610
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-03-04 19:40:39 +0300

    Обновление писем

--- Template/vft/woocommerce/emails/email-order-details.php
+++ Template/vft/woocommerce/emails/email-order-details.php
@@ -1,90 +1,90 @@
-<?php
-/**
- * Order details table shown in emails.
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-order-details.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates\Emails
- * @version 3.7.0
- */
+<div class="container">
+                <p class="shipping">Thank you for your order from Vitaforest. Once your package ships we will send you a
+                    tracking number. You can check the status of your order by logging into your account.
+                </p>
+                <p class="contact-notice">
+                    If you have questions about your order, you can email us at <a
+                        href="mailto:info@superfoods.pro">info@superfoods.pro</a> .
+                </p>
+                <div class="order">
+                    <h3 class="order__title">Your order №<span>60000006734</span></h3>
+                    <p class="order__time">Placed on <span>April 15, 2021</span> </p>
+                    <div class="order__info info">
+                        <div class="info__item">
+                            <h4 class="info__title">Billing info</h4>
+                            <p>Dave Peterson</p>
+                            <p>Hartford, st. Street 67, 78</p>
+                            <p>Connecticut, 634081</p>
+                            <p>United States</p>
+                            <p class="info__phone"><img src="" alt="">+ 45 (477) 223456</p>
+                        </div>
 
-defined( 'ABSPATH' ) || exit;
+                        <div class="info__item">
+                            <h4 class="info__title">Shipping info</h4>
+                            <p>Dave Peterson</p>
+                            <p>Hartford, st. Street 67, 78</p>
+                            <p>Connecticut, 634081</p>
+                            <p>United States</p>
+                            <p class="info__phone"><img src="" alt="">+ 45 (477) 223456</p>
+                        </div>
+                    </div>
+                    <div class="order__info info paym">
+                        <div class="info__item">
+                            <h4 class="info__title">Payment method</h4>
+                            <p>Check / Money order</p>
+                        </div>
 
-$text_align = is_rtl() ? 'right' : 'left';
+                        <div class="info__item">
+                            <h4 class="info__title">Shipping method</h4>
+                            <p>Flate rate — fixed</p>
+                        </div>
+                    </div>
+                    <div class="table-wrapper">
+                        <table class="order__table table">
 
-do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
+                            <thead>
+                                <tr>
+                                    <th class="table__name">items</th>
+                                    <th class="table__qty">Quantity</th>
+                                    <th class="table__price">Price</th>
+                                </tr>
+                            </thead>
 
-<h2>
-	<?php
-	if ( $sent_to_admin ) {
-		$before = '<a class="link" href="' . esc_url( $order->get_edit_order_url() ) . '">';
-		$after  = '</a>';
-	} else {
-		$before = '';
-		$after  = '';
-	}
-	/* translators: %s: Order ID. */
-	echo wp_kses_post( $before . sprintf( __( '[Order #%s]', 'woocommerce' ) . $after . ' (<time datetime="%s">%s</time>)', $order->get_order_number(), $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ) );
-	?>
-</h2>
-
-<div style="margin-bottom: 40px;">
-	<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
-		<thead>
-			<tr>
-				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
-				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			echo wc_get_email_order_items( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-				$order,
-				array(
-					'show_sku'      => $sent_to_admin,
-					'show_image'    => false,
-					'image_size'    => array( 32, 32 ),
-					'plain_text'    => $plain_text,
-					'sent_to_admin' => $sent_to_admin,
-				)
-			);
-			?>
-		</tbody>
-		<tfoot>
-			<?php
-			$item_totals = $order->get_order_item_totals();
-
-			if ( $item_totals ) {
-				$i = 0;
-				foreach ( $item_totals as $total ) {
-					$i++;
-					?>
-					<tr>
-						<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
-						<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
-					</tr>
-					<?php
-				}
-			}
-			if ( $order->get_customer_note() ) {
-				?>
-				<tr>
-					<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Note:', 'woocommerce' ); ?></th>
-					<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
-				</tr>
-				<?php
-			}
-			?>
-		</tfoot>
-	</table>
-</div>
-
-<?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
+                            <tbody>
+                                <tr>
+                                    <td class="table__name">Taxifolin-rich (Dahurian larch) dry extract</td>
+                                    <td class="table__qty">1</td>
+                                    <td class="table__price">€9.00</td>
+                                </tr>
+                                <tr>
+                                    <td class="table__name">Taxifolin-rich (Dahurian larch) dry extract</td>
+                                    <td class="table__qty">1</td>
+                                    <td class="table__price">€9.00</td>
+                                </tr>
+                                <tr>
+                                    <td class="table__name">Taxifolin-rich (Dahurian larch) dry extract</td>
+                                    <td class="table__qty">1</td>
+                                    <td class="table__price">€9.00</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr class='subtotal subtotal-first'>
+                                    <td class='table__fake'></td>
+                                    <td class='table__foot-header'>Subtotal:</td>
+                                    <td>€29.00</td>
+                                </tr>
+                                <tr class='subtotal'>
+                                    <td class='table__fake'></td>
+                                    <td class='table__foot-header'>Shipping:</td>
+                                    <td>€5.00</td>
+                                </tr>
+                                <tr class="total">
+                                    <td class='table__fake'></td>
+                                    <td class='table__total-header'>Order total:</td>
+                                    <td>€34.00</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
